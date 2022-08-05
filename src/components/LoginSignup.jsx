850a88b5f2594c40eef770c836b4fc8824a544b5
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-08-05 15:44:23 -0400

    updated

---
+++ src/components/LoginSignup.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './Login';
+import Signup from './Signup';
+
+
+const LoginSignup = () => {
+    return (
+        <div>
+            <div>Login_signup</div>
+            <Login />
+            <Signup />
+        </div>
+    )
+};
+export default LoginSignup;
---
+++ src/components/Signup.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+
+const Signup = () => {
+    let [username, setUsername] = useState("");
+    let [firstName, setFirstName] = useState("");
+    let [lastName, setLastName] = useState("");
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    let [confirm, setConfirm] = useState("");
+    let [formErrors, setFormErrors] = useState({});
+
+    const Register = (e) => {
+        let navigate = useNavigate();
+        e.preventDefault();
+        let formInfo = { username, firstName, lastName, email, password, confirm };
+        axios.post("http://localhost/api/users/signup", formInfo, { withCredentials: true })
+            .then(res => {
+                console.log(res);
+                if (res.data.errors) setFormErrors(res.data.errors);
+                else navigate("/dashboard");
+            })
+            .catch(e => console.log(e))
+    };
+
+    return (<>
+        <div>Signup</div>
+        <form onSubmit={Register}>
+            <div className="form-group">
+                <label htmlFor="">Username</label>
+                <input type="text" name="username" id="" className='form-control' onChange={(e) => setUsername(e.target.value)} />
+                <p className="text-danger">{formErrors.username?.message}</p>
+            </div>
+            <div className="form-group">
+                <label htmlFor="">First Name</label>
+                <input type="text" name="firstName" id="" className='form-control' onChange={(e) => setFirstName(e.target.value)} />
+                <p className="text-danger">{formErrors.firstName?.message}</p>
+            </div>
+            <div className="form-group">
+                <label htmlFor="">Last Name</label>
+                <input type="text" name="lastName" id="" className='form-control' onChange={(e) => setLastName(e.target.value)} />
+                <p className="text-danger">{formErrors.lastName?.message}</p>
+            </div>
+            <div className="form-group">
+                <label htmlFor="">Email</label>
+                <input type="text" name="email" id="" className='form-control' onChange={(e) => setEmail(e.target.value)} />
+                <p className="text-danger">{formErrors.email?.message}</p>
+            </div>
+            <div className="form-group">
+                <label htmlFor="">Password</label>
+                <input type="password" name="password" id="" className='form-control' onChange={(e) => setPassword(e.target.value)} />
+                <p className="text-danger">{formErrors.password?.message}</p>
+            </div>
+            <div className="form-group">
+                <label htmlFor="">Confirm Password</label>
+                <input type="password" name="confirm" id="" className='form-control' onChange={(e) => setConfirm(e.target.value)} />
+                <p className="text-danger">{formErrors.confirm?.message}</p>
+            </div>
+            <input type="submit" value="Register" className="btn btn-primary mt-3" />
+        </form>
+    </>)
+};
+export default Signup;
