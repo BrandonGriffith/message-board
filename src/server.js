acc5db022da44c5080c153c070940f7e033b7ef5
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-09-09 23:41:13 -0400

    updated

--- src/server.js
+++ src/server.js
@@ -58,8 +58,6 @@ app.use("/api/v1/users", userRouter);
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
+app.route('/*').get((req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 module.exports = app;
