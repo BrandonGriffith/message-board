1502044765822e81ac157d8c5f5ca3fac2945fd1
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-09-12 09:48:14 -0400

    update sameSite

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ const mySession = session({
     cookie: {
         secure: false,
         httpOnly: false,
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24 * 7
         },
     },
